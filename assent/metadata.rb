92f6c3fff56f44bcaba32eb0c8caa6e157032627
Brian Sullivan <brian@civicevolution.org>
2015-03-17 16:55:11 -0700

    remove ref to deploy

--- assent/metadata.rb
+++ assent/metadata.rb
@@ -6,8 +6,6 @@ description "Custom config for CE2"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "0.0.1"
 
-depends 'deploy'
-
 %w{ubuntu debian fedora suse amazon}.each do |os|
   supports os
 end
