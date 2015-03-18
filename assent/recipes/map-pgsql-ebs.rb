a9116bd0b5d7f9957e22650a9290b0d36fba7791
Brian Sullivan <brian@civicevolution.org>
2015-03-17 17:37:59 -0700

    check if mount is linked before making the link

--- assent/recipes/map-pgsql-ebs.rb
+++ assent/recipes/map-pgsql-ebs.rb
@@ -1,8 +1,8 @@
 
 #Chef::Log.info "do pretty print on node  #{}"
-Chef::Log.info "check for the mount_point node:  #{ JSON.pretty_generate(node) }"
+#Chef::Log.info "check for the mount_point node:  #{ JSON.pretty_generate(node) }"
 
-Chef::Log.info "Find mount_point  #{}"
+#Chef::Log.info "Find mount_point  #{}"
 #mount_point = node['ebs']['raids']['/dev/md0']['mount_point'] rescue nil
 #mount_point = node['ebs']['devices']['/dev/xvdi']['mount_point'] rescue nil
 
@@ -11,7 +11,7 @@ mount_point = node['deploy']['assent_prod']['database']['ebs_mount_point'] rescu
 app_owner = node['deploy']['assent_prod']['database']['username'] rescue nil
 
 Chef::Log.info "the mount_point IS   #{mount_point}"
-Chef::Log.info "app_owner  #{app_owner}"
+#Chef::Log.info "app_owner  #{app_owner}"
 
 if mount_point
   Chef::Log.info "Inside if mount_point with   #{mount_point}"
@@ -24,6 +24,7 @@ if mount_point
 
   link "/var/lib/pgsql93" do
     to "#{mount_point}"
+    not_if { ::File.directory?("/var/lib/pgsql93") }
   end
 end
 
