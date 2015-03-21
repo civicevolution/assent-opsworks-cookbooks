099e44572464d06bc59589937daa5a5996377c62
Brian Sullivan <brian@civicevolution.org>
2015-03-21 16:51:57 -0700

    debug info in deploy::default

--- deploy/recipes/default.rb
+++ deploy/recipes/default.rb
@@ -1,10 +1,20 @@
 include_recipe 'dependencies'
 
+Chef::Log.info("\n\n\nCustom deploy::default\n\n\n")
+
 node[:deploy].each do |application, deploy|
+
+  Chef::Log.info "\n\n$$$$$ pp deploy  #{}"
+  pp deploy
 
   opsworks_deploy_user do
 
+    Chef::Log.info "deploy[:user]  #{deploy[:user]}"
+
     if deploy[:application_type] == 'nodejs'
+
+      Chef::Log.info "Create opsworks.js with deploy[:database]  #{deploy[:database]}"
+
       template "#{deploy[:deploy_to]}/shared/config/opsworks.js" do
         cookbook 'opsworks_nodejs'
         source 'opsworks.js.erb'
